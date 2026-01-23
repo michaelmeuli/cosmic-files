46bd4583e3ec7d49f42799fe49379b260d17fc91
Michael Meuli <michael.meuli@gmail.com>
2026-01-23 12:09:15 +0100

    wip

--- src/russh/russh.rs
+++ src/russh/russh.rs
@@ -1025,9 +1025,18 @@ impl Russh {
                                     remote_file.path,
                                     path
                                 );
-                                let filename = Path::new(&remote_file.path)
-                                    .file_name()
-                                    .ok_or_else(|| anyhow!("remote path has no filename"))?;
+                                let filename = match Path::new(&remote_file.path).file_name() {
+                                    Some(name) => name.to_string_lossy().into_owned(),
+                                    None => {
+                                        if let Some(result_tx) = result_tx_opt.take() {
+                                            let _ = result_tx.send(Err(anyhow::anyhow!(
+                                                "remote path has no filename: {}",
+                                                remote_file.path
+                                            )));
+                                        }
+                                        continue;
+                                    }
+                                };
                                 if let Some(client) = existing_client {
                                     let result = client
                                         .download_file(remote_file.path, path.join(filename))
