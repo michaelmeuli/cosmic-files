7bdeb14be104cd4fe498afa3613169213d42ed56
Michael Meuli <michael.meuli@gmail.com>
2026-03-25 14:59:19 +0100

    Revert "clear all running_jobs"
    This reverts commit 9b5f34ddb58cd7c220cb1f2cbd2b7f7491499c7a.

--- src/app.rs
+++ src/app.rs
@@ -4116,10 +4116,8 @@ impl Application for App {
             Message::JobStatusUpdate(_client_key, _uri, array_id, running_tasks) => {
                 log::info!("Job {array_id} running tasks: {running_tasks}");
                 if running_tasks == 0 {
-                    // self.state.running_jobs.remove(&array_id);
-                    // self.state.job_total_tasks.remove(&array_id);
-                    self.state.running_jobs.clear();
-                    self.state.job_total_tasks.clear();
+                    self.state.running_jobs.remove(&array_id);
+                    self.state.job_total_tasks.remove(&array_id);
                 } else {
                     self.state.running_jobs.insert(array_id, running_tasks);
                 }
